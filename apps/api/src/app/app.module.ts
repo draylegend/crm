25394b095aedf311d8068fefa185a4367245186f
Vladimir Drayling <->
2022-03-16 16:54:11 +0100

    chore(packages): add gql

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,9 +1,17 @@
+import { ApolloDriver } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   controllers: [AppController],
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: true,
+      driver: ApolloDriver,
+    }),
+  ],
   providers: [AppService],
 })
 export class AppModule {}

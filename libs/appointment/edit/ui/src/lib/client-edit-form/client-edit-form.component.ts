4959683234ced4c1f90885b446b623ac2ab8c504
Vladimir Drayling <->
2022-03-22 15:59:40 +0100

    feat(appointment-edit-ui): filter client list

--- libs/appointment/edit/ui/src/lib/client-edit-form/client-edit-form.component.ts
+++ libs/appointment/edit/ui/src/lib/client-edit-form/client-edit-form.component.ts
@@ -1,6 +1,14 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { ControlContainer, FormGroupName } from '@angular/forms';
-import { ClientType } from '@crm/client/api';
+import { type ClientType } from '@crm/client/api';
+import {
+  BehaviorSubject,
+  map,
+  Observable,
+  pipe,
+  startWith,
+  UnaryFunction,
+} from 'rxjs';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -10,7 +18,29 @@ import { ClientType } from '@crm/client/api';
   viewProviders: [{ provide: ControlContainer, useExisting: FormGroupName }],
 })
 export class ClientEditFormComponent {
-  @Input() clients: ClientType[] | null = [];
+  filteredClients$!: Observable<ClientType[]>;
+  focus$ = new BehaviorSubject(false);
 
-  visible = false;
+  constructor(private readonly group: FormGroupName) {}
+
+  @Input() set clients(value: ClientType[]) {
+    this.filteredClients$ = this.group.control.valueChanges.pipe(
+      startWith(this.group.value.firstName),
+      this.#filter$(value),
+    );
+  }
+
+  #filter$(
+    clients: ClientType[],
+  ): UnaryFunction<Observable<unknown>, Observable<ClientType[]>> {
+    return pipe(
+      map(() =>
+        clients.filter(c =>
+          c.firstName
+            .toLowerCase()
+            .includes(this.group.value.firstName.toLowerCase()),
+        ),
+      ),
+    );
+  }
 }

f9dca0e15134dffbabae1e44f224a7f41b2f9c11
Vladimir Drayling <->
2022-05-31 01:07:31 +0200

    fix(workspace): postinstall & remove crm link

--- tools/postinstall.js
+++ tools/postinstall.js
@@ -2,6 +2,6 @@ const fs = require('fs');
 const { exec } = require('child_process');
 const envFile = '.env.example';
 
-fs.existsSync(envFile) && fs.copyFileSync(envFile, '.env');
+!fs.existsSync(envFile) && fs.copyFileSync(envFile, '.env');
 
 exec('prisma db push && prisma db seed');

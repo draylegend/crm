c4c05e7b8c98ce5b4d20f1bd5be35decd6de38e1
Vladimir Drayling <>
2022-08-19 09:11:08 +0200

    feat(shared-api): add prisma service

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,6 +1,7 @@
+import { SharedApiModule } from '@crm/shared/api';
 import { Module } from '@nestjs/common';
 
 @Module({
-  imports: [],
+  imports: [SharedApiModule],
 })
 export class AppModule {}
--- libs/shared/api/src/lib/shared-api.module.ts
+++ libs/shared/api/src/lib/shared-api.module.ts
@@ -1,7 +1,8 @@
-import { Module, Global } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
 
 @Global()
 @Module({
-  providers: [],
+  providers: [PrismaService],
 })
 export class SharedApiModule {}

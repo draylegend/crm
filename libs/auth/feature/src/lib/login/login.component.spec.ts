42b726f1b5f8557f2c41e6e61113f73da8b6b077
Vladimir Drayling <>
2022-08-30 21:23:45 +0200

    feat(auth-feature): add basic login form

--- libs/auth/feature/src/lib/login/login.component.spec.ts
+++ libs/auth/feature/src/lib/login/login.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './login.component';
 
 describe('LoginComponent', () => {
@@ -7,7 +8,7 @@ describe('LoginComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [LoginComponent],
+      imports: [LoginComponent, NoopAnimationsModule],
     });
 
     fixture = TestBed.createComponent(LoginComponent);
--- libs/auth/feature/src/lib/login/login.component.ts
+++ libs/auth/feature/src/lib/login/login.component.ts
@@ -1,10 +1,40 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  NonNullableFormBuilder,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+
+const EMAIL_PATTERN = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
+  styleUrls: ['./login.component.scss'],
   templateUrl: './login.component.html',
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    MatInputModule,
+    ReactiveFormsModule,
+  ],
 })
-export class LoginComponent {}
+export class LoginComponent {
+  form = this.fb.group({
+    email: ['', [Validators.required, Validators.pattern(EMAIL_PATTERN)]],
+    password: ['', [Validators.required]],
+  });
+
+  constructor(private fb: NonNullableFormBuilder) {}
+
+  login(): void {
+    console.log(this.form.value);
+  }
+}

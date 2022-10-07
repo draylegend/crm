41a53ddfbb3f05fc485fd6d74e0f7f04a1ccb948
Vladimir Drayling <draylegend@gmail.com>
2022-10-07 20:27:12 +0200

    refactor(lib): rename & move

--- apps/angular-admin/jest.config.ts
+++ apps/angular-admin/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'web-admin',
+  displayName: 'angular-admin',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
@@ -9,7 +9,7 @@ export default {
       stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  coverageDirectory: '../../coverage/apps/web-admin',
+  coverageDirectory: '../../coverage/apps/angular-admin',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
   },

d20a3ca1d89ae33ef72ead73f55f0758eec16545
Vladimir Drayling <draylegend@gmail.com>
2022-10-11 00:20:28 +0200

    feat(appointments-week): display hours rectangles

--- libs/appointments/week/feature/src/lib/appointments-week-feature.component.ts
+++ libs/appointments/week/feature/src/lib/appointments-week-feature.component.ts
@@ -1,8 +1,15 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostBinding } from '@angular/core';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
+  styleUrls: ['./appointments-week-feature.component.scss'],
   templateUrl: './appointments-week-feature.component.html',
 })
-export class AppointmentsWeekFeatureComponent {}
+export class AppointmentsWeekFeatureComponent {
+  @HostBinding('style') get style(): Record<string, string | number> {
+    return {
+      '--visibleDays': 7,
+    };
+  }
+}
